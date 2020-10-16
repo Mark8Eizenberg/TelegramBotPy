25e1ac6de474655c3cf2084e420aa2488538c277
Maxim Skorik <maksimskorik1999@gmail.com>
2020-10-17 01:43:46 +0300

    Add new method

--- tg_bot.py
+++ tg_bot.py
@@ -1,4 +1,15 @@
 import requests
+from enum import Enum
+import subprocess
+
+class Type_of_message(Enum):
+    TEXT = 1
+    STICKER = 2
+    AUDIO = 3
+    VIDEO = 4
+    PHOTO = 5
+    DOCUMENT = 6
+    VOICE = 7
 
 class TGBotHandler:
     #constructor
@@ -109,8 +120,46 @@ class TGBotHandler:
             parameters['reply_markup'] = reply_markup
         method = 'sendPhoto'
         return requests.post(self.url_api_addres + method, parameters)
-
+    
+    #return true if is private chat, false if group chat
+    def is_private_chat(self, last_update_data):
+        return (last_update_data['message']['chat']['type'] == 'private')
+    
+    #type of message
+    def get_type_of_message(self, last_update_data):
+        if('text' in last_update_data['message']):
+            return Type_of_message.TEXT
+        elif('sticker' in last_update_data['message']):
+            return Type_of_message.STICKER
+        elif('audio'in last_update_data['message']):
+            return Type_of_message.AUDIO
+        elif('video'in last_update_data['message']):
+            return Type_of_message.VIDEO
+        elif('photo'in last_update_data['message']):
+            return Type_of_message.PHOTO
+        elif('document'in last_update_data['message']):
+            return Type_of_message.DOCUMENT
+        elif('voice' in last_update_data['message']):
+            return Type_of_message.VOICE
         
+    #get file from chat
+    def get_file_link(self, file_id):
+        parameters = {'file_id':file_id}
+        method = 'getFile'
+        request = requests.post(self.url_api_addres + method, parameters).json()
+        return 'https://api.telegram.org/file/bot{}/'.format(self.token) + request['result']['file_path']
+        
+    #save file
+    def save_file_by_link(self, download_link:str):
+        link = requests.get(download_link)
+        link_split = download_link.split("/")
+        open(link_split[6], 'wb').write(link.content)
+        return link_split[6]
+
+    #convert oga to ogg
+    def convert_oga_to_ogg(self, file_path):
+        end_file = 'temp.ogg'
+        subprocess.run(['ffmpeg', '-i', file_path, end_file])
     #Other new methods
 
 bot = TGBotHandler("1141692325:AAGkNyHWLZX7HHqXVu_fpHAWT4jThvxqwbU")
@@ -123,20 +172,34 @@ def main():
         bot.get_updates(new_offset)
         last_update = bot.get_last_updates()
         if(last_update != None):
+            last_chat_text = 'text'
             last_update_id = last_update['update_id']
-            last_chat_text = last_update['message']['text']
             last_chat_id = last_update['message']['chat']['id']
+            if(bot.get_type_of_message(last_update) == Type_of_message.TEXT):
+                last_chat_text = last_update['message']['text']
+            elif(bot.get_type_of_message(last_update) == Type_of_message.STICKER):
+                bot.send_text_message(last_chat_id, 'you send me sticker')
+            elif(bot.get_type_of_message(last_update) == Type_of_message.VOICE):
+                bot.send_text_message(last_chat_id, 'you send me voice')
+                bot.save_file_by_link(bot.get_file_link(last_update['message']['voice']['file_id']))
+            elif(bot.get_type_of_message(last_update) == Type_of_message.DOCUMENT):
+                bot.send_text_message(last_chat_id, 'you send me file, nice :)')
+                bot.save_file_by_link(bot.get_file_link(last_update['message']['document']['file_id']))
             #last_chat_name = last_update['message']['chat']['first_name']
             #last_chat_message_id = last_update['message']['message_id']
             if(last_chat_text == '/sticker_test' or last_chat_text == '/sticker_test@my_157_test_bot'):
                 bot.send_sticker_exist(last_chat_id, sticker)
-            elif(last_chat_text.lower() == 'test'):
+            elif(last_chat_text.lower() == 'test' or last_chat_text.lower() == '/test'):
                 #bot.forward_message('', last_chat_id, last_chat_message_id,'true')
-                #bot.send_full_message(last_chat_id, 'Hi man!', None, None, 'true', last_update['message']['message_id'])
+                bot.send_full_message(last_chat_id, 'Hi man!', None, None, 'true', last_update['message']['message_id'])
                 #bot.send_text_message(last_chat_id, 'Сам сука, {}'.format(last_chat_name), last_update['message']['message_id'])
-                bot.send_text_HTML_message(last_chat_id,'<b>bold</b> <strong>bold</strong> <i>italic</i> <em>italic</em> <code>inline fixed-width code</code> <pre>pre-formatted fixed-width code block</pre> <a href="URL">inline URL</a> ')
+                #bot.send_text_HTML_message(last_chat_id,'<b>bold</b> <strong>bold</strong> <i>italic</i> <em>italic</em> <code>inline fixed-width code</code> <pre>pre-formatted fixed-width code block</pre> <a href="URL">inline URL</a> ')
                 #bot.send_sticker_exist(last_chat_id, sticker)
                 #bot.send_photo(last_chat_id, 'AgACAgIAAxkBAAIBCV-Ey_995dZELO37fGFnfHcTtRwqAALjrjEbcg0pSD9tm0F57UKppnFoly4AAwEAAwIAA3kAAzy9AQABGwQ')
+                if(bot.is_private_chat(last_update)):
+                    bot.send_text_message(last_chat_id, 'it is private')
+                else:
+                    bot.send_text_message(last_chat_id, 'is group')
             else:
                 bot.send_text_message(last_chat_id, '5 минут, полёт нормальный')
         new_offset = last_update_id + 1
